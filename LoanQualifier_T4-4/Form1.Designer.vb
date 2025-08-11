5781c677288be49ee8c4e08d0883fa8d04491fbc
mars <ammarsyazani3120@gmail.com>
2025-08-11 21:17:46 +0800

    Refactor Form1 layout and functionality
    Updated `Form1.Designer.vb` to remove `lblAnnualSalary` and `TextBox1`, adding new controls: `txtYearsOnJob`, `Label1`, `txtSalary`, `Label2`, `lblMessage`, `Label3`, `btnCheckQualifications`, `btnClear`, and `btnExit`. Adjusted layout properties and `ClientSize`.
    
    Enhanced `Form1.vb` with new event handlers for button actions, including loan qualification logic in `btnCheckQualifications_Click`, input clearing in `btnClear_Click`, and form closure in `btnExit_Click`. Added a `TextChanged` event for `txtSalary`.

--- LoanQualifier_T4-4/Form1.Designer.vb
+++ LoanQualifier_T4-4/Form1.Designer.vb
@@ -23,15 +23,24 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         GroupBox1 = New GroupBox()
-        lblAnnualSalary = New Label()
-        TextBox1 = New TextBox()
+        txtYearsOnJob = New TextBox()
+        Label1 = New Label()
+        txtSalary = New TextBox()
+        Label2 = New Label()
+        lblMessage = New TextBox()
+        Label3 = New Label()
+        btnCheckQualifications = New Button()
+        btnClear = New Button()
+        btnExit = New Button()
         GroupBox1.SuspendLayout()
         SuspendLayout()
         ' 
         ' GroupBox1
         ' 
-        GroupBox1.Controls.Add(TextBox1)
-        GroupBox1.Controls.Add(lblAnnualSalary)
+        GroupBox1.Controls.Add(txtYearsOnJob)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Controls.Add(txtSalary)
+        GroupBox1.Controls.Add(Label2)
         GroupBox1.Location = New Point(91, 88)
         GroupBox1.Name = "GroupBox1"
         GroupBox1.Size = New Size(608, 125)
@@ -39,37 +48,111 @@ Partial Class Form1
         GroupBox1.TabStop = False
         GroupBox1.Text = "Enter the following information: "
         ' 
-        ' lblAnnualSalary
+        ' txtYearsOnJob
         ' 
-        lblAnnualSalary.AutoSize = True
-        lblAnnualSalary.Location = New Point(100, 40)
-        lblAnnualSalary.Name = "lblAnnualSalary"
-        lblAnnualSalary.Size = New Size(104, 20)
-        lblAnnualSalary.TabIndex = 0
-        lblAnnualSalary.Text = "Annual salary: "
+        txtYearsOnJob.Location = New Point(309, 75)
+        txtYearsOnJob.Name = "txtYearsOnJob"
+        txtYearsOnJob.Size = New Size(148, 27)
+        txtYearsOnJob.TabIndex = 3
         ' 
-        ' TextBox1
+        ' Label1
         ' 
-        TextBox1.Location = New Point(210, 37)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(148, 27)
-        TextBox1.TabIndex = 1
+        Label1.AutoSize = True
+        Label1.Location = New Point(51, 82)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(252, 20)
+        Label1.TabIndex = 2
+        Label1.Text = "Number of years at your current job: "
+        ' 
+        ' txtSalary
+        ' 
+        txtSalary.Location = New Point(309, 33)
+        txtSalary.Name = "txtSalary"
+        txtSalary.Size = New Size(148, 27)
+        txtSalary.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(199, 40)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(104, 20)
+        Label2.TabIndex = 0
+        Label2.Text = "Annual salary: "
+        ' 
+        ' lblMessage
+        ' 
+        lblMessage.Location = New Point(158, 247)
+        lblMessage.Multiline = True
+        lblMessage.Name = "lblMessage"
+        lblMessage.ReadOnly = True
+        lblMessage.Size = New Size(500, 52)
+        lblMessage.TabIndex = 4
+        lblMessage.WordWrap = False
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(444, 291)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(0, 20)
+        Label3.TabIndex = 5
+        ' 
+        ' btnCheckQualifications
+        ' 
+        btnCheckQualifications.Location = New Point(119, 343)
+        btnCheckQualifications.Name = "btnCheckQualifications"
+        btnCheckQualifications.Size = New Size(165, 74)
+        btnCheckQualifications.TabIndex = 6
+        btnCheckQualifications.Text = "Check Qualifications"
+        btnCheckQualifications.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(334, 343)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(165, 74)
+        btnClear.TabIndex = 7
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btnExit
+        ' 
+        btnExit.Location = New Point(550, 343)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(165, 74)
+        btnExit.TabIndex = 8
+        btnExit.Text = "Exit"
+        btnExit.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(800, 447)
+        Controls.Add(btnExit)
+        Controls.Add(btnClear)
+        Controls.Add(btnCheckQualifications)
+        Controls.Add(Label3)
+        Controls.Add(lblMessage)
         Controls.Add(GroupBox1)
         Name = "Form1"
         Text = "Loan Qualifier"
         GroupBox1.ResumeLayout(False)
         GroupBox1.PerformLayout()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents lblAnnualSalary As Label
+    Friend WithEvents txtSalary As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtYearsOnJob As TextBox
+    Friend WithEvents lblMessage As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnCheckQualifications As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
 
 End Class
--- LoanQualifier_T4-4/Form1.vb
+++ LoanQualifier_T4-4/Form1.vb
@@ -1,3 +1,50 @@
 ﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtSalary.TextChanged
 
+    End Sub
+
+    Private Sub btnCheckQualifications_Click(sender As Object, e As EventArgs) Handles btnCheckQualifications.Click
+        'Variables to hold input data
+        Dim dblSalary As Double
+        Dim intYearsOnJob As Integer
+
+        Try
+            'Get the users input
+            dblSalary = CDbl(txtSalary.Text)
+            intYearsOnJob = CInt(txtYearsOnJob.Text)
+
+            'Determine wether the user qualifies for a loan
+            If dblSalary > 3000 Then
+                If intYearsOnJob > 2 Then
+                    lblMessage.Text = "The applicant qualifies."
+                Else
+                    lblMessage.Text = "The applicant does not qualify. "
+                End If
+            Else
+                If intYearsOnJob > 5 Then
+                    lblMessage.Text = "The applicant qualifies."
+                Else
+                    lblMessage.Text = "The applicant does not qualify."
+                End If
+            End If
+
+        Catch ex As Exception
+            'Display an error message
+            lblMessage.Text = "Please enter numeric values"
+
+        End Try
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'cLear the text boxes and label
+        txtSalary.Clear()
+        txtYearsOnJob.Clear()
+        lblMessage.Text = String.Empty
+        txtSalary.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close the form
+        Me.Close()
+    End Sub
 End Class
