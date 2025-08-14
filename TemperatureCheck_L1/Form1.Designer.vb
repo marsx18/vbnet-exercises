105f0a164723278c12b5562ecae32b98991b33f9
mars <ammarsyazani3120@gmail.com>
2025-08-14 22:23:54 +0800

    Initialize TemperatureCheck_L1 Windows Forms app
    Created a new Windows Forms application "TemperatureCheck_L1" with a main form (Form1) for temperature input and categorization. Added UI components for user interaction, including text boxes, labels, and buttons. Implemented logic for handling user input and actions. Included App.config for .NET runtime version, and generated solution and project files. Updated assembly metadata and added resource and settings management files for localization and user configurations.

---
+++ TemperatureCheck_L1/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtTemperature = New System.Windows.Forms.TextBox()
+        Me.lblCategory = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnCheck = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtTemperature
+        '
+        Me.txtTemperature.Location = New System.Drawing.Point(285, 109)
+        Me.txtTemperature.Multiline = True
+        Me.txtTemperature.Name = "txtTemperature"
+        Me.txtTemperature.Size = New System.Drawing.Size(171, 35)
+        Me.txtTemperature.TabIndex = 0
+        '
+        'lblCategory
+        '
+        Me.lblCategory.Location = New System.Drawing.Point(285, 200)
+        Me.lblCategory.Multiline = True
+        Me.lblCategory.Name = "lblCategory"
+        Me.lblCategory.ReadOnly = True
+        Me.lblCategory.Size = New System.Drawing.Size(171, 35)
+        Me.lblCategory.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(116, 112)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(145, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Enter your temperature:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(196, 203)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Category:"
+        '
+        'btnCheck
+        '
+        Me.btnCheck.Location = New System.Drawing.Point(151, 345)
+        Me.btnCheck.Name = "btnCheck"
+        Me.btnCheck.Size = New System.Drawing.Size(142, 53)
+        Me.btnCheck.TabIndex = 4
+        Me.btnCheck.Text = "Check"
+        Me.btnCheck.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(330, 345)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(142, 53)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(506, 345)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(142, 53)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCheck)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblCategory)
+        Me.Controls.Add(Me.txtTemperature)
+        Me.Name = "Form1"
+        Me.Text = "Temperature Checker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtTemperature As TextBox
+    Friend WithEvents lblCategory As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnCheck As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ TemperatureCheck_L1/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles lblCategory.TextChanged
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtTemperature.TextChanged
+
+    End Sub
+
+    Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+        'cHEthe temperature and set the category
+        Dim temperature As Double
+        If Double.TryParse(txtTemperature.Text, temperature) Then
+            If temperature < 20 Then
+
+                lblCategory.Text = "SEJUK"
+            ElseIf temperature >= 20 AndAlso temperature < 30 Then
+                lblCategory.Text = "SEDERHANA"
+            Else
+                lblCategory.Text = "PANAS"
+            End If
+        Else
+            lblCategory.Text = "SILA MASUKKAN NILAI YANG BETUL"
+        End If
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear the input and output
+        txtTemperature.Clear()
+        lblCategory.Text = String.Empty
+
+        txtTemperature.Focus()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'cLOSE THE FORM
+        Me.Close()
+    End Sub
+End Class
