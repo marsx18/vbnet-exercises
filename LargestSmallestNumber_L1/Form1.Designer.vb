782fcb206d03cbe2358340ab2d9c93687f0b8a9c
mars <ammarsyazani3120@gmail.com>
2025-08-14 22:05:13 +0800

    Create Windows Forms app for largest/smallest numbers
    This commit introduces the "LargestSmallestNumber_L1" Windows Forms application. It includes a main form (Form1) for user input of two numbers, along with functionality to calculate and display the smallest and largest values.
    
    Key changes:
    - Added `App.config` for .NET runtime version (v4.7.2).
    - Created `Form1.Designer.vb` for form layout and controls.
    - Implemented logic in `Form1.vb` for handling user interactions.
    - Added resource management with `Form1.resx`.
    - Established project structure with solution and project files.
    - Defined application settings in `Application.Designer.vb` and `Application.myapp`.
    - Updated assembly information in `AssemblyInfo.vb`.
    - Created resource and settings management files.
    
    These changes lay the foundation for a functional user interface and application logic.

---
+++ LargestSmallestNumber_L1/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFirstNumber = New System.Windows.Forms.TextBox()
+        Me.txtSecondNumber = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblSmallestNumber = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblLargestNumber = New System.Windows.Forms.TextBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtFirstNumber
+        '
+        Me.txtFirstNumber.Location = New System.Drawing.Point(307, 66)
+        Me.txtFirstNumber.Multiline = True
+        Me.txtFirstNumber.Name = "txtFirstNumber"
+        Me.txtFirstNumber.Size = New System.Drawing.Size(161, 44)
+        Me.txtFirstNumber.TabIndex = 0
+        Me.txtFirstNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtSecondNumber
+        '
+        Me.txtSecondNumber.Location = New System.Drawing.Point(307, 153)
+        Me.txtSecondNumber.Multiline = True
+        Me.txtSecondNumber.Name = "txtSecondNumber"
+        Me.txtSecondNumber.Size = New System.Drawing.Size(161, 44)
+        Me.txtSecondNumber.TabIndex = 1
+        Me.txtSecondNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(175, 82)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(112, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Enter first number:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(150, 167)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(137, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Enter second number:"
+        '
+        'lblSmallestNumber
+        '
+        Me.lblSmallestNumber.Location = New System.Drawing.Point(307, 232)
+        Me.lblSmallestNumber.Multiline = True
+        Me.lblSmallestNumber.Name = "lblSmallestNumber"
+        Me.lblSmallestNumber.ReadOnly = True
+        Me.lblSmallestNumber.Size = New System.Drawing.Size(161, 32)
+        Me.lblSmallestNumber.TabIndex = 4
+        Me.lblSmallestNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(177, 235)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(110, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Smallest number:"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(572, 66)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(148, 65)
+        Me.btnCalculate.TabIndex = 6
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(572, 264)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(148, 65)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(175, 300)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(106, 16)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Largest Number:"
+        '
+        'lblLargestNumber
+        '
+        Me.lblLargestNumber.Location = New System.Drawing.Point(307, 297)
+        Me.lblLargestNumber.Multiline = True
+        Me.lblLargestNumber.Name = "lblLargestNumber"
+        Me.lblLargestNumber.ReadOnly = True
+        Me.lblLargestNumber.Size = New System.Drawing.Size(161, 32)
+        Me.lblLargestNumber.TabIndex = 9
+        Me.lblLargestNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(572, 167)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(148, 65)
+        Me.btnClear.TabIndex = 10
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lblLargestNumber)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblSmallestNumber)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtSecondNumber)
+        Me.Controls.Add(Me.txtFirstNumber)
+        Me.Name = "Form1"
+        Me.Text = "Largest and Smallest Number Finder"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtFirstNumber As TextBox
+    Friend WithEvents txtSecondNumber As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblSmallestNumber As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblLargestNumber As TextBox
+    Friend WithEvents btnClear As Button
+End Class
---
+++ LargestSmallestNumber_L1/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'Find the first number in the text box
+        Dim firstNumber As Double
+        If Double.TryParse(txtFirstNumber.Text, firstNumber) Then
+            'Find the second number in the text box
+            Dim secondNumber As Double
+            If Double.TryParse(txtSecondNumber.Text, secondNumber) Then
+                'Find the smallest number
+                Dim smallestNumber As Double = Math.Min(firstNumber, secondNumber)
+                'Find the largest number
+                Dim largestNumber As Double = Math.Max(firstNumber, secondNumber)
+                'Display the results
+                lblSmallestNumber.Text = smallestNumber.ToString()
+                lblLargestNumber.Text = largestNumber.ToString()
+            Else
+                MessageBox.Show("Please enter a valid second number.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Else
+            MessageBox.Show("Please enter a valid first number.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear the text boxes and labels
+        txtFirstNumber.Clear()
+        txtFirstNumber.Focus()
+        txtSecondNumber.Clear()
+        lblSmallestNumber.Text = String.Empty
+        lblLargestNumber.Text = String.Empty
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close the form
+        Me.Close()
+    End Sub
+End Class
