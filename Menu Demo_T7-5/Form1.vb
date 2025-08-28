d634010090994e12b1a83689fd10942d23769ff6
mars <ammarsyazani3120@gmail.com>
2025-08-28 14:48:05 +0800

    Added a Menu demo project for chapter 5 Exercise

---
+++ Menu Demo_T7-5/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+    Private Sub mnuFileExit_Click(sender As Object, e As EventArgs) Handles mnuFileExit.Click
+        'Close the fom
+        Me.Close()
+    End Sub
+
+    Private Sub mnuColorRed_Click(sender As Object, e As EventArgs) Handles mnuColorRed.Click
+        'Set the label's foreground color to Red
+        lblMessage.ForeColor = Color.Red
+
+    End Sub
+
+    Private Sub mnuColorGreen_Click(sender As Object, e As EventArgs) Handles mnuColorGreen.Click
+        'Set the label's foreground color to Green
+        lblMessage.ForeColor = Color.Green
+    End Sub
+
+    Private Sub mnuColorBlue_Click(sender As Object, e As EventArgs) Handles mnuColorBlue.Click
+        'Set the label's foreground color to Blue
+        lblMessage.ForeColor = Color.Blue
+
+    End Sub
+
+    Private Sub mnuColorBlack_Click(sender As Object, e As EventArgs) Handles mnuColorBlack.Click
+        'Set the label's foreground color to Black
+        lblMessage.ForeColor = Color.Black
+    End Sub
+
+    Private Sub mnuColorVisible_Click(sender As Object, e As EventArgs) Handles mnuColorVisible.Click
+        'Make the label visible or invisible
+        If mnuColorVisible.Checked = True Then
+            lblMessage.Visible = True
+        Else
+            lblMessage.Visible = False
+        End If
+
+    End Sub
+
+    Private Sub mnuHelpAbout_Click(sender As Object, e As EventArgs) Handles mnuHelpAbout.Click
+        'Display a simple about box
+        MessageBox.Show("A Simple Menu System Demo")
+    End Sub
+End Class
---
+++ Menu Demo_T7-5/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Menu Demo_T7-5")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Menu Demo_T7-5")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4ec83f7b-6cda-490a-9da9-b56e3e394a27")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
