04aa2b77dd48f900ff2f67c753862469aa000c4e
mars <ammarsyazani3120@gmail.com>
2025-09-22 23:47:00 +0800

    Add lottery number generator Windows Forms application
    This commit introduces a new Windows Forms application for generating lottery numbers. Key changes include:
    
    - Added App.config to define application configuration.
    - Created Form1.Designer.vb for the main form layout and controls.
    - Implemented lottery number generation logic in Form1.vb.
    - Updated project files (sln and vbproj) to include the new form and resources.
    - Added resource files (Form1.resx and Resources.resx) for localization.
    - Updated AssemblyInfo.vb with new application metadata.

---
+++ Lottery Numbers_T8-1/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblFirst = New System.Windows.Forms.Label()
+        Me.lblSecond = New System.Windows.Forms.Label()
+        Me.lblThird = New System.Windows.Forms.Label()
+        Me.lblFourth = New System.Windows.Forms.Label()
+        Me.lblFifth = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(301, 222)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(110, 61)
+        Me.btnGenerate.TabIndex = 5
+        Me.btnGenerate.Text = "&Generate Numbers"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(429, 222)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(110, 61)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblFirst
+        '
+        Me.lblFirst.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblFirst.Location = New System.Drawing.Point(217, 138)
+        Me.lblFirst.Name = "lblFirst"
+        Me.lblFirst.Size = New System.Drawing.Size(69, 38)
+        Me.lblFirst.TabIndex = 7
+        Me.lblFirst.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblSecond
+        '
+        Me.lblSecond.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSecond.Location = New System.Drawing.Point(301, 138)
+        Me.lblSecond.Name = "lblSecond"
+        Me.lblSecond.Size = New System.Drawing.Size(69, 38)
+        Me.lblSecond.TabIndex = 8
+        Me.lblSecond.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblThird
+        '
+        Me.lblThird.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblThird.Location = New System.Drawing.Point(386, 138)
+        Me.lblThird.Name = "lblThird"
+        Me.lblThird.Size = New System.Drawing.Size(69, 38)
+        Me.lblThird.TabIndex = 9
+        Me.lblThird.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblFourth
+        '
+        Me.lblFourth.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblFourth.Location = New System.Drawing.Point(470, 138)
+        Me.lblFourth.Name = "lblFourth"
+        Me.lblFourth.Size = New System.Drawing.Size(69, 38)
+        Me.lblFourth.TabIndex = 10
+        Me.lblFourth.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblFifth
+        '
+        Me.lblFifth.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblFifth.Location = New System.Drawing.Point(555, 138)
+        Me.lblFifth.Name = "lblFifth"
+        Me.lblFifth.Size = New System.Drawing.Size(69, 38)
+        Me.lblFifth.TabIndex = 11
+        Me.lblFifth.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblFifth)
+        Me.Controls.Add(Me.lblFourth)
+        Me.Controls.Add(Me.lblThird)
+        Me.Controls.Add(Me.lblSecond)
+        Me.Controls.Add(Me.lblFirst)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Name = "Form1"
+        Me.Text = "Lottery Numbers"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblFirst As Label
+    Friend WithEvents lblSecond As Label
+    Friend WithEvents lblThird As Label
+    Friend WithEvents lblFourth As Label
+    Friend WithEvents lblFifth As Label
+End Class
---
+++ Lottery Numbers_T8-1/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Const intMAX_SUBSCRIPT As Integer = 4
+        Dim intLottery(intMAX_SUBSCRIPT) As Integer
+        Dim intCount As Integer
+
+        'Create a Random object.
+        Dim rand As New Random
+
+        'Fill the array with random numbers.
+        'Each number will be range 0-99
+        For intCount = 0 To intMAX_SUBSCRIPT
+            intLottery(intCount) = rand.Next(0, 100)
+        Next
+        'Display the array elements in the labels
+        lblFirst.Text = intLottery(0).ToString
+        lblSecond.Text = intLottery(1).ToString
+        lblThird.Text = intLottery(2).ToString
+        lblFourth.Text = intLottery(3).ToString
+        lblFifth.Text = intLottery(4).ToString
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close the form
+        Me.Close()
+
+    End Sub
+End Class
