26b8c517e0e829d4fba7895beb56693d11b1c0ad
mars <ammarsyazani3120@gmail.com>
2025-09-23 10:11:44 +0800

    Add Windows Forms application for file creation demo
    This commit introduces a new Visual Studio solution and project for a Windows Forms application named "File WriteLine Demo". Key changes include:
    
    - Creation of `File WriteLine Demo.sln` and `File WriteLine Demo.vbproj` to manage project configurations and properties.
    - Addition of `Form1.Designer.vb` for the main form layout, including controls for user interaction.
    - Implementation of application logic in `Form1.vb`, allowing users to input friend information and save it to a file.
    - Inclusion of supporting files such as `Application.Designer.vb`, `AssemblyInfo.vb`, and resource files to ensure proper application configuration and resource management.
    
    These changes establish a complete application with a user interface for creating and saving data files.

---
+++ File WriteLine DemoT9-6/File WriteLine Demo/Form1.vb
@@ -0,0 +1,49 @@
+﻿
+Imports System.IO
+
+Structure FriendInfo
+    Dim strName As String
+    Dim strPhone As String
+End Structure
+Public Class Form1
+    Private Sub btnCreateFile_Click(sender As Object, e As EventArgs) Handles btnCreateFile.Click
+        Const intNUM_FRIENDS As Integer = 3
+
+        'Local variables
+        Dim strFileName As String
+        Dim intCount As Integer
+        Dim friendFile As StreamWriter
+        Dim myFriend As FriendInfo
+
+        'Get the file name.
+        strFileName = InputBox("Enter the filename. ")
+
+        Try
+            'Open the file.
+            friendFile = File.CreateText(strFileName)
+
+            'Get the friends' information and write it to the file.
+            For intCount = 1 To intNUM_FRIENDS
+                'Get the friend's name.
+                myFriend.strName = InputBox("Enter the name of friend " & "number " & intCount.ToString())
+                'get the friend's phone number.
+                myFriend.strPhone = InputBox("Enter that friend's phone number. ")
+
+                'Write date to the file.]
+                friendFile.WriteLine(myFriend.strName)
+                friendFile.WriteLine(myFriend.strPhone)
+
+            Next
+            'Close the file.   
+            friendFile.Close()
+        Catch
+            MessageBox.Show("That file cannot be created. ")
+
+        End Try
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close the form.
+        Me.Close()
+    End Sub
+End Class
