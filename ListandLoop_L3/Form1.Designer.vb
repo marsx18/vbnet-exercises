80aa1da4519d4255c72f0431cd47ce28831e65d8
mars <ammarsyazani3120@gmail.com>
2025-10-02 22:50:32 +0800

    Initialize Windows Forms application with core files
    This commit sets up a new Windows Forms application, including the necessary configuration files and resources. Key changes include the addition of `App.config` for .NET runtime specification, `Form1.Designer.vb` for the main form layout, and `Form1.vb` for rental cost calculation logic. The project structure is organized with solution and project files (`ListandLoop_L3.sln` and `ListandLoop_L3.vbproj`), along with auto-generated files for application settings, resources, and assembly information.

---
+++ ListandLoop_L3/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtDays = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Items.AddRange(New Object() {"Viva", "MyVi", "Alza", "Saga", "Persona"})
+        Me.ListBox1.Location = New System.Drawing.Point(191, 48)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(114, 100)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(97, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Pilih kereta:"
+        '
+        'txtDays
+        '
+        Me.txtDays.Location = New System.Drawing.Point(179, 170)
+        Me.txtDays.Name = "txtDays"
+        Me.txtDays.Size = New System.Drawing.Size(134, 22)
+        Me.txtDays.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(45, 173)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(128, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Bilangan Hari Sewa:"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(184, 286)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(121, 56)
+        Me.btnCalculate.TabIndex = 5
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(32, 232)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(141, 16)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Jumlah perlu dibayar : "
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(179, 232)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.ReadOnly = True
+        Me.txtTotal.Size = New System.Drawing.Size(134, 22)
+        Me.txtTotal.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtDays)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "List and Loop (Lab Skill 3)"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtDays As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtTotal As TextBox
+End Class
---
+++ ListandLoop_L3/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim hargaSehari As Double = 0
+        Dim jumlah As Double = 0
+        Dim hari As Integer
+
+        'Semak kalau tiada kereta dipilih
+        If ListBox1.SelectedIndex = -1 Then
+            MessageBox.Show("Sila pilih jenis kereta.")
+            Exit Sub
+        End If
+
+        'Semak input bilangan hari
+        If Not Integer.TryParse(txtDays.Text, hari) Or hari <= 0 Then
+            MessageBox.Show("Sila masukkan bilangan hari yang sah.")
+            Exit Sub
+        End If
+
+        'Tetapkan harga berdasarkan pilihan kereta
+        Select Case ListBox1.SelectedItem.ToString()
+            Case "Viva"
+                hargaSehari = 100
+            Case "MyVi"
+                hargaSehari = 150
+            Case "Alza"
+                hargaSehari = 170
+            Case "Saga"
+                hargaSehari = 120
+            Case "Persona"
+                hargaSehari = 150
+        End Select
+
+        'Kira jumlah
+        jumlah = hargaSehari * hari
+
+        'Paparkan dalam textbox jumlah
+        txtTotal.Text = "RM " & jumlah.ToString("F2")
+    End Sub
+End Class
