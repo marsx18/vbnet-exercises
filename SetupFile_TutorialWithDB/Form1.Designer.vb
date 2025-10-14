cd092bbe671c5439d5220e165fbb40d6027fe992
mars <ammarsyazani3120@gmail.com>
2025-10-14 23:31:04 +0800

    Chapter 9 Database tutorial

---
+++ SetupFile_TutorialWithDB/Form1.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        DataGridView1 = New DataGridView()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 16F)
+        Label1.ForeColor = Color.White
+        Label1.Location = New Point(309, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(191, 37)
+        Label1.TabIndex = 0
+        Label1.Text = "User Info Form"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 12F)
+        Label2.ForeColor = Color.White
+        Label2.Location = New Point(82, 70)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(31, 28)
+        Label2.TabIndex = 1
+        Label2.Text = "ID"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 12F)
+        Label3.ForeColor = Color.White
+        Label3.Location = New Point(82, 116)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(64, 28)
+        Label3.TabIndex = 2
+        Label3.Text = "Name"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 12F)
+        Label4.ForeColor = Color.White
+        Label4.Location = New Point(82, 165)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(82, 28)
+        Label4.TabIndex = 3
+        Label4.Text = "Address"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 12F)
+        Label5.ForeColor = Color.White
+        Label5.Location = New Point(82, 210)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(47, 28)
+        Label5.TabIndex = 4
+        Label5.Text = "Age"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 12F)
+        TextBox1.Location = New Point(193, 70)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(148, 34)
+        TextBox1.TabIndex = 5
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Font = New Font("Segoe UI", 12F)
+        TextBox2.Location = New Point(193, 116)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(148, 34)
+        TextBox2.TabIndex = 6
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Font = New Font("Segoe UI", 12F)
+        TextBox3.Location = New Point(193, 162)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(148, 34)
+        TextBox3.TabIndex = 7
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Font = New Font("Segoe UI", 12F)
+        TextBox4.Location = New Point(193, 210)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(148, 34)
+        TextBox4.TabIndex = 8
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.Blue
+        Button1.Font = New Font("Segoe UI", 12F)
+        Button1.ForeColor = Color.White
+        Button1.Location = New Point(158, 270)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 37)
+        Button1.TabIndex = 9
+        Button1.Text = "Add"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.Blue
+        Button2.Font = New Font("Segoe UI", 12F)
+        Button2.ForeColor = Color.White
+        Button2.Location = New Point(284, 270)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 37)
+        Button2.TabIndex = 10
+        Button2.Text = "Load"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(82, 347)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.Size = New Size(628, 222)
+        DataGridView1.TabIndex = 11
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Green
+        ClientSize = New Size(800, 593)
+        Controls.Add(DataGridView1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+
+End Class
---
+++ SetupFile_TutorialWithDB/Form1.vb
@@ -0,0 +1,54 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim id As Integer = Integer.Parse(TextBox1.Text)
+            Dim name As String = TextBox2.Text
+            Dim address As String = TextBox3.Text
+            Dim age As Integer = Integer.Parse(TextBox4.Text)
+
+            Dim query As String = "INSERT INTO UserInfo_Tab (id, name, address, age) VALUES (@id, @name, @address, @age)"
+
+            Using con As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Databases\MyAttachedDB.mdf;Integrated Security=True;Connect Timeout=30")
+                Using cmd As New SqlCommand(query, con)
+                    cmd.Parameters.AddWithValue("@id", id)
+                    cmd.Parameters.AddWithValue("@name", name)
+                    cmd.Parameters.AddWithValue("@address", address)
+                    cmd.Parameters.AddWithValue("@age", age)
+
+                    con.Open()
+                    cmd.ExecuteNonQuery()
+                    BindData()
+                End Using
+            End Using
+
+            MessageBox.Show("Record inserted successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+    Private Sub BindData()
+        Try
+            Dim query As String = "SELECT * FROM UserInfo_Tab"
+
+            Using con As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Databases\MyAttachedDB.mdf;Integrated Security=True;Connect Timeout=30")
+                Using cmd As New SqlCommand(query, con)
+                    Using sda As New SqlDataAdapter(cmd)
+                        Dim dt As New DataTable()
+                        sda.Fill(dt)
+                        DataGridView1.DataSource = dt
+                    End Using
+                End Using
+            End Using
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        BindData()
+    End Sub
+End Class
